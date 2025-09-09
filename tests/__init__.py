0beff1d12e22ecc45b19d58bc43ea0a349c2dc1d
d4g33z <jeff@du.local>
2025-09-09 19:44:36 -0400

    update tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -41,10 +41,10 @@ def debug_on(*exceptions):
         return wrapper
     return decorator
 
-# from tests.tree import TestTree
-# from tests.transformers import TestTransformer
-# from tests.validators import TestValidator
-# from tests.evaluators import TestEvaluator
+from tests.tree import TestTree
+from tests.transformers import TestTransformer
+from tests.validators import TestValidator
+from tests.evaluators import TestEvaluator
 
 if __name__ == '__main__':
     _tl = unittest.TestLoader()
